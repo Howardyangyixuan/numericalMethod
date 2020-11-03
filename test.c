06fdf122f7c905b67dd369e5e639fdc69704a1ab
howardyangyixuan <howardyangyixuan@gmail.com>
2020-11-03 08:37:29 +0800

    完成向量操作，并测试通过

--- test.c
+++ test.c
@@ -1,9 +1,105 @@
-#include "myVector.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#define ElemType double
+#define ERROR -2
+#define MISMATCH -3  //(维数)不匹配
+#define UNORDERED -4 //不能用顺序法求解
+#define STRANGE 5    //矩阵奇异
+#define E 1.0e-2     //相对误差限
+
+typedef struct
+{
+    int dimension;  //维数
+    ElemType *elem; //向量,一维数组
+} Vector;
+
+typedef struct
+{
+    int dimension; //矩阵维数
+    int s;         //上半带宽
+    int r;         //下半带宽
+    double **elem; //矩阵，二维数组
+} Matrix_diag;     //n维s+1+r对角方阵
+
+//输入&V，n；初始化n维向量V（用1~n):
+void VectorInit(Vector *v, int n)
+{
+    v->dimension = n;
+
+    v->elem = (ElemType *)malloc(sizeof(ElemType) * (n + 1));
+    return;
+}
+
+//复制向量
+void VectorCopy(Vector V, Vector *x)
+{
+    if (V.dimension != x->dimension)
+        exit(MISMATCH);
+    for (int i = 1; i <= V.dimension; ++i)
+    {
+        (x->elem)[i] = V.elem[i];
+    }
+    return;
+}
+
+//打印向量
+void PrintVector(Vector V)
+{
+    for (int i = 1; i <= V.dimension; ++i)
+    {
+        printf("%lf ", V.elem[i]);
+    }
+    printf("\n");
+    return;
+}
+
+//向量数乘，V=向量U*数k（要求V已经初始化），传入V的指针，改变V后，返回V的指针
+Vector *Vector_Num(Vector U, double k, Vector *v)
+{
+    if (U.dimension == v->dimension)
+    {
+        for (int i = 1; i <= U.dimension; ++i)
+        {
+            v->elem[i] = U.elem[i] * k;
+        }
+    }
+    else
+    {
+        exit(MISMATCH);
+    }
+    return v;
+}
+
+//向量内积
+double DotProduct(Vector X, Vector Y)
+{
+    if (X.dimension == Y.dimension)
+    {
+        double p = 0;
+        for (int i = 1; i <= X.dimension; ++i)
+        {
+            p += X.elem[i] * Y.elem[i];
+        }
+        return p;
+    }
+    else
+    {
+        exit(MISMATCH);
+    }
+}
 int main()
 {
     Vector a;
-    VectorInit(&a, 1);
+    VectorInit(&a, 5);
     a.elem[1] = 1;
+    a.elem[5] = 5;
     PrintVector(a);
+    Vector b;
+    VectorInit(&b, 5);
+    Vector_Num(a, 2, &b);
+    PrintVector(b);
+    printf("dot product a,b: %lf", DotProduct(a, b));
+    printf("dot product b,a: %lf", DotProduct(b, a));
     return 0;
 }
