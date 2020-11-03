149c5df89cd12062f964a7894bfbbd592b365852
howardyangyixuan <howardyangyixuan@gmail.com>
2020-11-03 16:51:47 +0800

    debug幂法，*写成了+，并完成测试，得到第一问的最大和最小值

--- testTransPM.c
+++ testTransPM.c
@@ -188,7 +188,7 @@ double PM_eigenvalue(Matrix_diag A)
         v = Vector_Num(U, 1 / norm, &V);
         //等价于Vector_Num(U, 1 / norm, &V);
         U.elem[1] = a[1] * V.elem[1] + b * V.elem[2] + c * V.elem[3];
-        U.elem[2] = b + V.elem[1] + a[2] * V.elem[2] + b * V.elem[3] + c * V.elem[4];
+        U.elem[2] = b * V.elem[1] + a[2] * V.elem[2] + b * V.elem[3] + c * V.elem[4];
         for (i = 3; i <= 499; ++i)
         {
             U.elem[i] = a[i] * V.elem[i] + b * (V.elem[i - 1] + V.elem[i + 1]) + c * (V.elem[i - 2] + V.elem[i + 2]);
@@ -199,11 +199,63 @@ double PM_eigenvalue(Matrix_diag A)
         lambda_new = DotProduct(V, U);
         e = fabs(lambda_new - lambda_old) / fabs(lambda_new); //求相对误差
         ++count;
-        printf("epoch : %d, error : %.12lf\n", count, e);
+        // printf("epoch : %d, error : %.12e\n", count, e);
     }
     free(V.elem);
     free(U.elem);
     return lambda_new;
+}
+
+double PMTran_eigenvalue(Matrix_diag A, double p)
+{
+    double b = 0.16;
+    double c = -0.064;
+    double a[502];
+    int i, j;
+    for (j = 1; j <= 501; ++j)
+    {
+        a[j] = (1.64 - 0.024 * j) * sin(0.2 * j) - 0.64 * exp(0.1 / j) - p;
+    }
+    //定义变量
+    const int n = A.dimension;
+    const int r = A.r;
+    const int s = A.s;
+    const int m = s + 1 + r;
+    int count, k;
+    double norm, e, lambda_old, lambda_new = 0;
+    Vector U;
+    Vector *u = &U;
+    VectorInit(&U, n);
+    Vector V;
+    Vector *v = &V;
+    VectorInit(&V, n);
+    for (i = 1; i <= n; ++i)
+    {
+        U.elem[i] = 1;
+    }
+    e = 1;
+    count = 0;
+    while (e > E)
+    {
+        norm = sqrt(DotProduct(U, U));
+        v = Vector_Num(U, 1 / norm, &V);
+        U.elem[1] = a[1] * V.elem[1] + b * V.elem[2] + c * V.elem[3];
+        U.elem[2] = b * V.elem[1] + a[2] * V.elem[2] + b * V.elem[3] + c * V.elem[4];
+        for (i = 3; i <= 499; ++i)
+        {
+            U.elem[i] = a[i] * V.elem[i] + b * (V.elem[i - 1] + V.elem[i + 1]) + c * (V.elem[i - 2] + V.elem[i + 2]);
+        }
+        U.elem[500] = b * V.elem[501] + a[500] * V.elem[500] + b * V.elem[499] + c * V.elem[498];
+        U.elem[501] = a[501] * V.elem[501] + b * V.elem[500] + c * V.elem[499];
+        lambda_old = lambda_new;
+        lambda_new = DotProduct(V, U);
+        e = fabs(lambda_new - lambda_old) / fabs(lambda_new + p); //求相对误差
+        ++count;
+        // printf("epoch : %d, error : %.12e\n", count, e);
+    }
+    free(V.elem);
+    free(U.elem);
+    return lambda_new + p;
 }
 int main()
 {
@@ -212,6 +264,8 @@ int main()
     int s = 2;
     int r = 2;
     Matrix_diagInit(&a, n, s, r);
-    printf("PM result: %.12e", PM_eigenvalue(a));
+    double tmp = PM_eigenvalue(a);
+    printf("PM result: %.12e\n", PM_eigenvalue(a));
+    printf("PMTrans result: %.12e\n", PMTran_eigenvalue(a, tmp));
     return 0;
 }
